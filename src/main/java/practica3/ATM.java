095125cc8c19f235e5341c5ddc778107474475e4
Georgie <gbenevento@cifpfbmoll.eu>
2021-05-29 22:57:45 +0200

    Practica 3 si

---
+++ src/main/java/practica3/ATM.java
@@ -0,0 +1,137 @@
+package practica3;
+
+import java.util.ArrayList;
+
+public class ATM {
+
+    static Integer idLastATM = 0;
+    private Integer idATM;
+    private int[][] bills;
+    private ArrayList<Card> cards = new ArrayList<>();
+
+    //Constructores
+
+    public ATM() {
+    }
+
+    public ATM(int[][] money, ArrayList<Card> cards) {
+        setIdATM(++idLastATM);
+        setBills(money);
+        setCards(cards);
+        setIdLastATM(this.idATM);
+    }
+
+    public ATM(ATM copyATM) {
+        this.idATM = copyATM.idATM;
+        this.bills = copyATM.bills;
+        this.cards = copyATM.cards;
+    }
+
+    //Setters y getters
+
+    public static Integer getIdLastATM() {
+        return idLastATM;
+    }
+
+    public static void setIdLastATM(Integer idLastATM) {
+        ATM.idLastATM = idLastATM;
+    }
+
+    public Integer getIdATM() {
+        return idATM;
+    }
+
+    public void setIdATM(Integer idATM) {
+        this.idATM = idATM;
+    }
+
+    public int[][] getBills() {
+        return bills;
+    }
+
+    public void setBills(int[][] bills) {
+        this.bills = bills;
+    }
+
+    public ArrayList<Card> getCards() {
+        return cards;
+    }
+
+    public void setCards(ArrayList<Card> cards) {
+        this.cards = cards;
+    }
+
+    //Metodos
+
+    public void showATMBalance() { // Imprime los billetes del cajero
+        System.out.println("==============================");
+        System.out.println("|      - ATM  BALANCE -      |");
+        System.out.println("==============================");
+        System.out.println("There is " + bills[0][1] + " 500 € bills");
+        System.out.println("There is " + bills[1][1] + " 200 € bills");
+        System.out.println("There is " + bills[2][1] + " 100 € bills");
+        System.out.println("There is " + bills[3][1] + " 50 € bills");
+        System.out.println("There is " + bills[4][1] + " 20 € bills");
+        System.out.println("There is " + bills[5][1] + " 10 € bills");
+        System.out.println("There is " + bills[6][1] + " 5 € bills");
+        System.out.println("------------------------------");
+        System.out.println("A total balance of " + this.checkATMBalance() + " €");
+        System.out.println("==============================");
+    }
+
+    public Integer checkATMBalance() { //Devuelve el valor exacto de dinero que queda en el cajero
+        int balance = 0;
+        for (int i = 0; i < this.getBills().length; i++) {
+            balance += this.getBills()[i][1] * this.getBills()[i][0];
+        }
+        return balance;
+    }
+
+    public boolean authentication(String NIF, Integer PIN) { //Comprueba si el NIF y el PIN son correctos
+        boolean authentication = false, unknownNIF = true;
+        for (int i = 0; i < this.getCards().size(); i++) {
+            Card ourCard = this.getCards().get(i);
+            if (ourCard.getNIF().equals(NIF)) {
+                unknownNIF = false;
+                if (ourCard.getPIN().equals(PIN)) {
+                    authentication = true;
+                } else {
+                    System.out.println(" ");
+                    System.out.println("Incorrect PIN for NIF: " + NIF);
+                }
+            }
+        }
+        if (unknownNIF) {
+            System.out.println(" ");
+            System.out.println("Unknown NIF");
+        }
+        return authentication;
+    }
+
+    public void takeMoney(Integer amount) { // Saca billetes del cajero y imprime que billetes se han sacado
+        int cont = 0;
+        int[][] bills = this.getBills();
+        int[][] taken = new int[][]{{500, 0}, {200, 0}, {100, 0}, {50, 0}, {20, 0}, {10, 0}, {5, 0}};
+        for (int i = 0; i < bills.length; i++) {
+            for (int j = 0; j < bills[i][1]; j++) {
+                if (amount > 0 && bills[i][0] <= amount) {
+                    if (bills[i][1] > 0) {
+                        amount = amount - bills[i][0];
+                        taken[i][1] += 1;
+                        cont++;
+                    }
+                }
+            }
+            this.bills[i][1] -= cont;
+            cont = 0;
+        }
+        System.out.println("****************************");
+        for (int i = 0; i < taken.length; i++) {
+            if (taken[i][1] > 0) {
+
+                System.out.println(taken[i][1] + " bills of " + taken[i][0] + " € were taken");
+            }
+        }
+        System.out.println("****************************");
+    }
+}
---
+++ src/main/java/practica3/CreditCard.java
@@ -0,0 +1,72 @@
+package practica3;
+
+public class CreditCard extends Card {
+    private Integer availableBalance;
+    private Integer availableCredit;
+
+    //Constructores
+
+    public CreditCard() {
+
+    }
+
+    public CreditCard(String NIF, Integer PIN, String name, String surname, Integer availableBalance, Integer availableCredit) {
+        setNIF(NIF);
+        setPIN(PIN);
+        setName(name);
+        setSurname(surname);
+        setActive(true);
+        this.availableBalance = availableBalance;
+        this.availableCredit = availableCredit;
+        setType("CREDIT");
+    }
+
+    public CreditCard(CreditCard copyCard) {
+        setNIF(copyCard.getNIF());
+        setPIN(copyCard.getPIN());
+        setName(copyCard.getName());
+        setSurname(copyCard.getSurname());
+        setActive(copyCard.isActive());
+        setType(copyCard.getType());
+        this.availableBalance = copyCard.availableBalance;
+        this.availableCredit = copyCard.availableCredit;
+    }
+
+    //Setters y getters
+
+    public Integer getAvailableBalance() {
+        return availableBalance;
+    }
+
+    public void setAvailableBalance(Integer availableBalance) {
+        this.availableBalance = availableBalance;
+    }
+
+    public Integer getAvailableCredit() {
+        return availableCredit;
+    }
+
+    public void setAvailableCredit(Integer availableCredit) {
+        this.availableCredit = availableCredit;
+    }
+
+    //Metodos
+
+    @Override //Override del metodo showCard que proviene de la clase Card
+    public void showCard() { //Imprime los detalles del cliente especificados
+        System.out.println("==========================");
+        System.out.println("|    -CLIENT DETAILS-    |");
+        System.out.println("==========================");
+        System.out.println("Name: " + this.getName());
+        System.out.println("Surname: " + this.getSurname());
+        System.out.println("NIF: " + this.getNIF());
+        System.out.println("Available Credit: " + this.getAvailableCredit()+ " €");
+        System.out.println("Available Balance: " + this.getAvailableBalance()+ " €");
+        System.out.println("==========================");
+
+    }
+
+    public int totalAvailable() { //Devuelve el saldo + el credito
+        return (this.availableBalance + this.availableCredit);
+    }
+}
